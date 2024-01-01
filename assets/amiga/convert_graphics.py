2e27219ca26c1fe9da7bb0b15d6059e5ba2932b1
Jean-François Fabre <jotd666@gmail.com>
2024-01-01 23:53:19 +0100

    support for HW sprites (BOBs not done yet)

--- assets/amiga/convert_graphics.py
+++ assets/amiga/convert_graphics.py
@@ -1,4 +1,4 @@
-import os,re,bitplanelib,ast,json
+import os,re,bitplanelib,ast
 from PIL import Image,ImageOps
 
 
@@ -38,7 +38,7 @@ if dump_sprites:
     ensure_empty(uncategorized_dump_sprites_dir)
 
 
-NB_POSSIBLE_SPRITES = 128  #64+64 alternate
+NB_POSSIBLE_SPRITES = 128
 
 rw_json = os.path.join(this_dir,"used_cluts.json")
 if os.path.exists(rw_json):
@@ -47,7 +47,7 @@ if os.path.exists(rw_json):
     # key as integer, list as set for faster lookup (not that it matters...)
     used_cluts = {int(k):set(v) for k,v in used_cluts.items()}
 else:
-    print("Warning: no {} file, no tile/clut filter, expect BIG graphics.68k file")
+    print(f"Warning: no {rw_json} file, no tile/clut filter, expect BIG graphics.68k file")
     used_cluts = None
 
 
@@ -56,7 +56,6 @@ def dump_asm_bytes(*args,**kwargs):
     bitplanelib.dump_asm_bytes(*args,**kwargs,mit_format=True)
 
 
-opposite = {"left":"right","right":"left"}
 
 block_dict = {}
 
@@ -98,39 +97,38 @@ sprite_palette = [tuple(x) for x in block_dict['sprite_palette']["data"]]
 sprite_cluts = [[sprite_palette[i] for i in clut] for clut in block_dict['sprite_clut']["data"]]
 
 
-def dump_rgb_cluts(rgb_cluts,name):
-    out = os.path.join(dump_dir,f"{name}_cluts.png")
-    w = 16
-    nb_clut_per_row = 4
-    img = Image.new("RGB",(w*(nb_clut_per_row+1)*4,w*len(rgb_cluts)//nb_clut_per_row))
-    x = 0
-    y = 0
-    row_count = 0
-    for clut in rgb_cluts:
-        # undo the clut correction so it's the same as MAME
-        for color in [clut[0],clut[2],clut[1],clut[3]]:
-            for dx in range(w):
-                for dy in range(w):
-                    img.putpixel((x+dx,y+dy),color)
-            x += dx
-        row_count += 1
-        if row_count == 4:
-            x = 0
-            y += dy
-            row_count = 0
-
-    img.save(out)
-
-
+##def dump_rgb_cluts(rgb_cluts,name):
+##    out = os.path.join(dump_dir,f"{name}_cluts.png")
+##    w = 16
+##    nb_clut_per_row = 4
+##    img = Image.new("RGB",(w*(nb_clut_per_row+1)*4,w*len(rgb_cluts)//nb_clut_per_row))
+##    x = 0
+##    y = 0
+##    row_count = 0
+##    for clut in rgb_cluts:
+##        # undo the clut correction so it's the same as MAME
+##        for color in [clut[0],clut[2],clut[1],clut[3]]:
+##            for dx in range(w):
+##                for dy in range(w):
+##                    img.putpixel((x+dx,y+dy),color)
+##            x += dx
+##        row_count += 1
+##        if row_count == 4:
+##            x = 0
+##            y += dy
+##            row_count = 0
+##
+##    img.save(out)
 
 
-### dump cluts as RGB4 for sprites
-##with open(os.path.join(src_dir,"palette_cluts.68k"),"w") as f:
-##    for the_type,rgb_cluts in [("normal",rgb_cluts_normal),("alt",rgb_cluts_alt)]:
-##        f.write(f"{the_type}_cluts:")
-##        for clut in rgb_cluts:
-##            rgb4 = [bitplanelib.to_rgb4_color(x) for x in clut]
-##            bitplanelib.dump_asm_bytes(rgb4,f,mit_format=True,size=2)
+
+
+# dump cluts as RGB4 for sprites
+with open(os.path.join(src_dir,"palette_cluts.68k"),"w") as f:
+    f.write(f"sprite_cluts:")
+    for clut in sprite_cluts:
+        rgb4 = [bitplanelib.to_rgb4_color(x) for x in clut]
+        bitplanelib.dump_asm_bytes(rgb4,f,mit_format=True,size=2)
 
 
 tile_global_palette = sorted(set(tile_palette))
@@ -141,7 +139,7 @@ with open(os.path.join(src_dir,"palette.68k"),"w") as f:
 
 character_codes_list = []
 
-if False:
+if True:
     for k,chardat in enumerate(block_dict["tile"]["data"]):
         img = Image.new('RGB',(8,8))
 
@@ -168,25 +166,37 @@ def get_sprite_clut(clut_index):
 # creating the sprite configuration in the code is more flexible than with a config file
 
 sprite_config = dict()
+attached_sprites = set()
 jeep_cluts = {0,12}
 
 def add_sprite_block(start,end,prefix,cluts,is_sprite):
     if isinstance(cluts,int):
         cluts = [cluts]
     for i in range(start,end+1):
-        sprite_config[i] = {"name":f"{prefix}_{i:02x}","cluts":cluts,"is_sprite":True}
+        sprite_config[i] = {"name":f"{prefix}_{i:02x}","cluts":cluts,"is_sprite":is_sprite}
 
-add_sprite_block(0xA,0x10,"falling_jeep_part_",jeep_cluts,True)
-add_sprite_block(1,4,"jeep_part_",jeep_cluts,True)
-add_sprite_block(5,8,"jeep_wheel_",0,True)  # todo extract black vs background
+
+def group_vertically(sprite1,sprite2):
+    sprite_config[sprite1]["vattached"] = block_dict["sprite"]["data"][sprite2]
+    attached_sprites.add(sprite2)
+
+# jeep is made of sprites: 5 sprites needed, 6 color slots
+# so we can allow 2 more sprites but they must have same CLUT...
+# missile qualifies (made of 2 sprites)
+# tank qualifies (there can be 2 tanks at once)
+
+add_sprite_block(0x9,0x10,"falling_jeep",jeep_cluts,True)
+add_sprite_block(1,4,"jeep_part",jeep_cluts,True)
+add_sprite_block(5,8,"jeep_wheel",0,True)
 add_sprite_block(0x42,0x42,"saucer",7,False)
 add_sprite_block(0x43,0x44,"hole_making_ship",7,False)
 add_sprite_block(0x45,0x47,"ovoid_ship",7,False)
-add_sprite_block(0x38,0x38,"tank",7,False)
-add_sprite_block(0x3A,0x3B,"missile",9,False)  # todo extract black
-add_sprite_block(0x7B,0x7C,"missile",9,False)  # todo extract black
-add_sprite_block(0x7E,0x7E,"score_800",14,False)
-add_sprite_block(0x7D,0x7D,"score_500",14,False)
+add_sprite_block(0x38,0x38,"tank",7,True)
+add_sprite_block(0x3A,0x3B,"missile",9,True)
+add_sprite_block(0x7B,0x7C,"missile",9,True)
+add_sprite_block(0x7E,0x7E,"points",{14,15},False)  # 800,1000
+add_sprite_block(0x7D,0x7D,"points",{14,15},False)  # 300,500
+
 add_sprite_block(0x31,0x34,"rock_ball",4,False)
 add_sprite_block(0x36,0x37,"rock_ball",4,False)
 add_sprite_block(0x40,0x41,"medium_explosion",1,False)
@@ -197,30 +207,77 @@ add_sprite_block(0x48,0x4A,"ship_explosion",1,False)
 add_sprite_block(0x7A,0x7A,"small_explosion",1,False)
 add_sprite_block(0x61,0x6F,"ground_explosion",3,False)
 add_sprite_block(0x4B,0x4D,"ship_bomb",3,False)
-add_sprite_block(0x11,0x27,"jeep_explosion",{1,13},False)
+add_sprite_block(0x11,0x27,"jeep_explosion",{1,13},True)
 add_sprite_block(0x3D,0x3D,"mine",{3,10},False)
+add_sprite_block(0x39,0x39,"tank_shot",4,True)   # maybe wrong clut
+add_sprite_block(0x3C,0x3C,"missile_shot",4,False)   # maybe wrong clut
+add_sprite_block(0,0,"blank",0,False)   # maybe wrong clut
+add_sprite_block(0x28,0x28,"jeep_shot",14,False)   # maybe wrong clut
+add_sprite_block(0x35,0x35,"ground_digging_bomb",14,False)   # maybe wrong clut
+
+# group jeep sprites to save sprites
+group_vertically(0x9,0xb)
+group_vertically(0xa,0xc)
+group_vertically(0xd,0xf)
+group_vertically(0xe,0x10)
+group_vertically(0x1,0x3)
+group_vertically(0x2,0x4)
 
 transparent = (60,100,200)
 
-# brutal dump of all sprites in all cluts
+bobs_used_colors = collections.Counter()
+sprites_used_colors = collections.Counter()
+
+sprites = dict()
+
 for k,sprdat in enumerate(block_dict["sprite"]["data"]):
+    if k in attached_sprites:
+        sprites[k] = True   # note that sprite is legal but will be ignored
+        continue
+
     sprconf = sprite_config.get(k)
     if sprconf:
         clut_range = sprconf["cluts"]
         name = sprconf["name"]
+        is_sprite = sprconf["is_sprite"]
+        vattached = sprconf.get("vattached")
     else:
-        clut_range = range(0,15)
+        clut_range = range(0,16)
         name = f"unknown_{k:02x}"
+        is_sprite = False
+        vattached = None
 
     for cidx in clut_range:
-        img = Image.new('RGB',(16,16),transparent)
+        hsize = 32 if vattached else 16
+        img = Image.new('RGB',(16,hsize),transparent)
         spritepal = get_sprite_clut(cidx)
         spritepal[0] = transparent
         d = iter(sprdat)
         for j in range(16):
             for i in range(16):
                 v = next(d)
-                img.putpixel((i,j),spritepal[v])
+                color = spritepal[v]
+                if sprconf:
+                    (sprites_used_colors if is_sprite else bobs_used_colors)[color] += 1
+                img.putpixel((i,j),color)
+        if vattached:
+            d = iter(vattached)
+            for j in range(16,32):
+                for i in range(16):
+                    v = next(d)
+                    color = spritepal[v]
+                    (sprites_used_colors if is_sprite else bobs_used_colors)[color] += 1
+                    img.putpixel((i,j),color)
+
+        # only consider sprites/cluts which are pre-registered
+        if sprconf:
+            if is_sprite:
+                sdata = bitplanelib.palette_image2sprite(img,None,spritepal,
+                        palette_precision_mask=0xFF,sprite_fmode=0,with_control_words=True)
+            else:
+                sdata = bitplanelib.palette_image2raw(img,None,spritepal,forced_nb_planes=4,
+                    palette_precision_mask=0xFF,generate_mask=True,blit_pad=True,mask_color=transparent)
+            sprites[k] = {"is_sprite":is_sprite,"name":name,"bitmap":sdata,"hsize":hsize}
 
         if dump_sprites:
             scaled = ImageOps.scale(img,2,0)
@@ -229,65 +286,8 @@ for k,sprdat in enumerate(block_dict["sprite"]["data"]):
             else:
                 scaled.save(os.path.join(uncategorized_dump_sprites_dir,f"sprites_{k:02x}_{cidx}.png"))
 
-if False:
-
-
-
-    sprites = collections.defaultdict(dict)
-
-    clut_index = 12  # temp
-
-    bg_cluts_bank_0 = [[tuple(palette[pidx]) for pidx in clut] for clut in bg_cluts[0:16]]
-    # second bank
-    bg_cluts_bank_1 = [[tuple(palette[pidx]) for pidx in clut] for clut in bg_cluts[16:]]
-
-
-    # pick a clut index with different colors
-    # it doesn't matter which one
-    for clut in bg_cluts:
-        if len(clut)==len(set(clut)):
-            spritepal = clut
-            break
-    else:
-        # can't happen
-        raise Exception("no way jose")
-
-    # convert our picked palette to RGB
-    spritepal = [tuple(palette[pidx]) for pidx in spritepal]
-
-    for k,data in sprite_config.items():
-        sprdat = block_dict["sprite"]["data"][k]
-
-        d = iter(sprdat)
-        img = Image.new('RGB',(16,16))
-        y_start = 0
-
-
-        for i in range(16):
-            for j in range(16):
-                v = next(d)
-                if j >= y_start:
-                    img.putpixel((j,i),spritepal[v])
-
-        spr = sprites[k]
-        spr["name"] = data['name']
-        mirror = any(x in data["name"] for x in ("left","right"))
-
-        right = None
-        outname = f"{k:02x}_{clut_index}_{data['name']}.png"
-
-        left = bitplanelib.palette_image2sprite(img,None,spritepal)
-        if mirror:
-            right = bitplanelib.palette_image2sprite(ImageOps.mirror(img),None,spritepal)
-
-        spr["left"] = left
-        spr["right"] = right
-
-        if dump_it:
-            scaled = ImageOps.scale(img,5,0)
-            scaled.save(os.path.join(dump_dir,outname))
-
-
+print("Bobs colors: {}".format(len(bobs_used_colors)))
+print("Sprites colors: {}".format(len(sprites_used_colors)))
 
 with open(os.path.join(src_dir,"graphics.68k"),"w") as f:
     f.write("\t.global\tcharacter_table\n")
@@ -314,8 +314,7 @@ with open(os.path.join(src_dir,"graphics.68k"),"w") as f:
                 if cc is not None:
                     f.write(f"char_{i}_{j}:")
                     bitplanelib.dump_asm_bytes(cc,f,mit_format=True)
-    import sys
-    sys.exit(0)
+
     f.write("sprite_table:\n")
 
     sprite_names = [None]*NB_POSSIBLE_SPRITES
@@ -323,51 +322,38 @@ with open(os.path.join(src_dir,"graphics.68k"),"w") as f:
         sprite = sprites.get(i)
         f.write("\t.long\t")
         if sprite:
-            name = f"{sprite['name']}_{i:02x}"
-            sprite_names[i] = name
-            f.write(name)
+            if sprite == True:
+                f.write("-1")  # not displayed but legal
+            else:
+                if sprite["is_sprite"]:
+                    name = sprite['name']
+                    sprite_names[i] = name
+                    f.write(name)
+                else:
+                    f.write("0")
         else:
             f.write("0")
         f.write("\n")
 
     for i in range(NB_POSSIBLE_SPRITES):
-        sprite = sprites.get(i)
-        if sprite:
-            name = sprite_names[i]
+        name = sprite_names[i]
+        if name:
             f.write(f"{name}:\n")
             for j in range(8):
                 f.write("\t.long\t")
                 f.write(f"{name}_{j}")
                 f.write("\n")
 
-    for i in range(NB_POSSIBLE_SPRITES):
-        sprite = sprites.get(i)
-        if sprite:
-            name = sprite_names[i]
-            for j in range(8):
-                f.write(f"{name}_{j}:\n")
-
-                for d in ["left","right"]:
-                    bitmap = sprite[d]
-                    if bitmap:
-                        f.write(f"\t.long\t{name}_{j}_sprdata\n".replace(d,opposite[d]))
-                    else:
-                        # same for both
-                        f.write(f"\t.long\t{name}_{j}_sprdata\n")
 
     f.write("\t.section\t.datachip\n")
 
-    for i in range(256):
-        sprite = sprites.get(i)
-        if sprite:
-            name = sprite_names[i]
+    for i in range(NB_POSSIBLE_SPRITES):
+        name = sprite_names[i]
+        if name:
+            sprite = sprites.get(i)
             for j in range(8):
                 # clut is valid for this sprite
-
-                for d in ["left","right"]:
-                    bitmap = sprite[d]
-                    if bitmap:
-                        sprite_label = f"{name}_{j}_sprdata".replace(d,opposite[d])
-                        f.write(f"{sprite_label}:\n\t.long\t0\t| control word")
-                        bitplanelib.dump_asm_bytes(sprite[d],f,mit_format=True)
-                        f.write("\t.long\t0\n")
+                bitmap = sprite["bitmap"]
+                sprite_label = f"{name}_{j}"
+                f.write(f"{sprite_label}:\n\t.word\t{sprite['hsize']}")
+                bitplanelib.dump_asm_bytes(bitmap,f,mit_format=True)
