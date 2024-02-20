165411986c978a247b75bebbef5796b0df4ef541
Jean-François Fabre <jotd666@gmail.com>
2024-02-20 22:55:28 +0100

    fixed 2 bugs

--- src/amiga/mpatrolHD.s
+++ src/amiga/mpatrolHD.s
@@ -36,7 +36,9 @@ _expmem
     dc.w    _config-_base
 ;---
 _config
-	dc.b	"C1:X:invincibility:0;
+	dc.b	"C1:X:invincibility:0;"
+	dc.b	"C1:X:infinite lives:1;"
+	dc.b	"C1:X:sector selection:3;"
 ;	dc.b    "C4:L:difficulty:easy,medium,hard,hardest;"
 ;	dc.b    "C5:L:start lives:default,2,3,4,5;"
 	IFD		CHIP_ONLY
