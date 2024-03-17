73d726082116e1f377ec6acf3f60b90b2d1fdd46
Jean-François Fabre <jotd666@gmail.com>
2024-03-17 01:15:15 +0100

    itch release

--- assets/amiga/convert_sounds.py
+++ assets/amiga/convert_sounds.py
@@ -44,7 +44,7 @@ sound_dict = {
 "MAIN_TUNE_SND"                :{"index":10,"pattern":1,"loops":True,"volume":40},
 "COURSE_START_SND"              :{"index":11,"pattern":0,"loops":False,"volume":40},
 "SECTION_END_SND"              :{"index":12,"pattern":5,"loops":False,"volume":40,"ticks":250},
-"COURSE_END_SND"              :{"index":13,"pattern":4,"loops":False,"volume":40,"ticks":500},
+"COURSE_END_SND"              :{"index":13,"pattern":4,"loops":False,"volume":40,"ticks":480},
 "GAME_OVER_SND"              :{"index":14,"pattern":6,"loops":False,"volume":40,"ticks":200},
 
 }
--- tools/create_amiga_archive.py
+++ tools/create_amiga_archive.py
@@ -23,7 +23,7 @@ for file in ["readme.md",gamename,f"{gamename}.slave"]:
     shutil.copy(os.path.join(progdir,file),outdir)
 
 shutil.copy(os.path.join(progdir,"assets","amiga","MoonPatrolGlowIcon.info"),outdir)
-#shutil.copy(os.path.join(progdir,"assets","amiga","boxart.png"),outdir)
+shutil.copy(os.path.join(progdir,"assets","amiga","boxart.png"),outdir)
 
 # pack the file for floppy
 subprocess.check_output(["cranker_windows.exe","-f",os.path.join(progdir,gamename),"-o",os.path.join(progdir,f"{gamename}.rnc")])
